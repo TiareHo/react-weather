4e06d9172f368e430c8e654858fc73decb5c7519
TiareHo <tiarehoegerman@gmail.com>
2022-08-08 14:33:42 -0700

    Fixed typo and H1 spacing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Weather Report App</h1>
       
       <SearchForm />
-      <p>Coded by <a href="https://www.linkedin.com/in/tiare-elise/" target="_blank" rel="noopener noreferrer">Tiare</a> using React + open-sourced via 
+      <p>Coded by <a href="https://www.linkedin.com/in/tiare-elise/" target="_blank" rel="noopener noreferrer">Tiare</a> using React + open-sourced via  
       <a href="https://github.com/TiareHo/react-weather" target="_blank" rel="noopener noreferrer">GitHub</a></p>
     </div>
   );
