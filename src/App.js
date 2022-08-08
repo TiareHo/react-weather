816b349c2b3c8e21177e55f19dc7bfcd9e2f28d4
TiareHo <tiarehoegerman@gmail.com>
2022-08-08 14:28:21 -0700

    Added links to open source code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <h1>Weather Report App</h1>
       
       <SearchForm />
-      <p>Coded by Tiare using React + open-sourced via GitHub</p>
+      <p>Coded by <a href="https://www.linkedin.com/in/tiare-elise/" target="_blank" rel="noopener noreferrer">Tiare</a> using React + open-sourced via 
+      <a href="https://github.com/TiareHo/react-weather" target="_blank" rel="noopener noreferrer">GitHub</a></p>
     </div>
   );
 }
